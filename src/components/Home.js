ae673aa8e7ae55ad140552c9ebcff00ea459fece
Garrett Tordo <76716199+PrinceFluffHK@users.noreply.github.com>
2023-11-20 14:54:23 -0500

    init commit

---
+++ src/components/Home.js
@@ -0,0 +1,32 @@
+import React from "react";
+import elder from "../assets/images/shutterstock_elderly-walk.jpeg";
+
+const Home = (props) => {
+    return (
+        <div>
+            <div className="cover-container">
+                <img
+                    className="cover-photo"
+                    src={elder}
+                    alt="https://www.shutterstock.com/image-photo/adult-granddaguhter-supporting-her-senior-grandmother-2172757295"
+                />
+            </div>
+            <div className="mission-container">
+                <h1 className="our-mission">Our Mission</h1>
+                <div className="hl"></div>
+                <h3>
+                    Create the first approved hydrogen inhalation therapy
+                    system, and improve the lives of the millions of people
+                    impacted reperfusion injury caused by stroke and cardiac
+                    arrest
+                </h3>
+                <p>
+                    HydroSpire is a research-stage company that currently has no
+                    approved products available for human use
+                </p>
+            </div>
+        </div>
+    );
+};
+
+export default Home;
